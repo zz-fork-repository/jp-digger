050f166f59b0a41d875f80de0047498e24a007d8
tama3 <tama3@acee48c3-7b26-0410-bdac-b3d0e5314bbc>
2008-09-12 08:46:31 +0000

    initial version
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/stigmata/digger/trunk@303 acee48c3-7b26-0410-bdac-b3d0e5314bbc

---
+++ src/main/java/jp/sourceforge/stigmata/digger/ClassFileArchive.java
@@ -0,0 +1,30 @@
+package jp.sourceforge.stigmata.digger;
+
+/*
+ * $Id$
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
+
+/**
+ * abstract presentation of class file archive's location.
+ *
+ * @author Haruaki TAMADA
+ * @version $Revision$ 
+ */
+public interface ClassFileArchive extends Iterable<ClassFileEntry>{
+    public URL getLocation();
+
+    public InputStream getInputStream(ClassFileEntry entry) throws IOException;
+
+    public Iterator<ClassFileEntry> iterator();
+
+    public boolean hasEntry(String className);
+
+    public ClassFileEntry getEntry(String className) throws ClassNotFoundException;
+
+    public String getName();
+}
